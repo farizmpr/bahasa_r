fd2628f61b15787b40d2baf32b15df12ca9be8e1
Farizpurnomo <farizpurnomo91@gmail.com>
2019-07-24 15:18:19 +0700

    Update prediksi_umur_menggunakan_average.R
    menggannti salary yang na dengan rata rata perhitungan kolom dari prediksi
    . baris 9 hanya ingin mencoba di ubah menjadi nilai true false, setelah itu kodingan eksekusi

--- prediksi_umur_menggunakan_average.R
+++ prediksi_umur_menggunakan_average.R
@@ -5,3 +5,9 @@ show(prediksi_dataset)
 prediksi_dataset$age= ifelse(is.na(prediksi_dataset$age),
                              ave(prediksi_dataset$age, FUN = function(x) mean(x,na.rm = TRUE))
                              ,prediksi_dataset$age)
+#mengganti dataset pada kolom salary dengan rata rata
+coba_uang <- is.na(prediksi_dataset$salary)
+show(coba_uang)
+prediksi_dataset$salary=ifelse(is.na(prediksi_dataset$salary),ave(prediksi_dataset$salary, 
+                                                                  FUN = function(y) mean(y,na.rm = TRUE)),
+                               prediksi_dataset$salary)
